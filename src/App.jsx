d676d1d77c9328723ec21c8b81d8319cfd3003ae
alejandro <alexgeorgegeer@gmail.com>
2024-03-05 00:43:53 -0600

    edited nav to hanburger icon, built top of about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 // import { useEffect, useState } from "react";
-import {  Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { About, Contact, Portfolio, Resume } from "./pages";
 import { Footer, Header, Nav } from "./components";
-
 
 export default function App() {
   const currentPage = useLocation().pathname;
@@ -11,17 +10,16 @@ export default function App() {
     <>
       <Header className="header" currentPage={currentPage}>
         {/* <Nav currentPage={currentPage} /> */}
-      </Header >
+      </Header>
       <main>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-
+        {/* <div className="container"> */}
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        {/* </div> */}
       </main>
       <Footer />
     </>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,16 @@
 export default function About() {
-   return (
+  return (
     <>
-    About
+      <section className="about-main">
+        <div className="about-main__main">
+          <h1 className="heading-1">Alex Geer</h1>
+          <h2 className="heading-2">
+            Full-stack web developer. <br />
+            I bring stories to life <br />
+            through web technology
+          </h2>
+        </div>
+      </section>
     </>
-   )
+  );
 }
